08b9303e4ad441705498d2289df53c9670dc6c4d
Andrew <nimandrey22@gmail.com>
2026-06-23 17:20:33 +0900

    Add: foes initializer

--- main.c
+++ main.c
@@ -49,14 +49,44 @@ Rectangle RectangleFromVector2(Vector2 position, Vector2 size){
     return rect;
 };
 
+void init_foes(Foe* a, int len, Vector2 cords, int margin, int foes_in_row){
+    int items_in_row;
+    if(len-1 > foes_in_row){
+        items_in_row = foes_in_row;
+    } else {
+        items_in_row = len-1;
+    }
+
+    int foes_rec_width = items_in_row * (foes_in_row + margin) - margin;
+    float centered = cords.x / 2.0f - foes_rec_width / 2.0f;
+    int row = 0;
+
+    for(int i = 0; i < len; i++){
+        if(i % foes_in_row == 0 && i != 0) {
+            row += 1;
+        }
+        a[i] = (Foe){
+            .size.x = 20,
+            .size.y = 20,
+            .health = 20,
+            .position.x = (foes_in_row + margin) * (i % foes_in_row) + centered,
+            .position.y = 40 * row,
+            .velocity = 1
+        };
+    }
+}
+
 int main(void) {
     const int screenWidth = 800;
     const int screenHeight = 400;
     //const char *text = "Welcome Invader";
     float shootTimer = 0;
-     Bullet bullets[100] = {0};
+    Bullet bullets[100] = {0};
+    Foe foes[60];
 
-     Player player = {
+    init_foes(foes, sizeof(foes) / sizeof(foes[0]), (Vector2){screenWidth, screenHeight}, 20, 15);
+
+    Player player = {
         .position = {.x = 0, .y = 0},
         .size = {.x = 20, .y = 20},
         .velocity = 5
@@ -73,7 +103,6 @@ int main(void) {
     
     SetTargetFPS(60);
 
-    //int textWidth = MeasureText(text, 16);
     player.position.x = screenWidth / 2.0f - player.size.x / 2.0f;
     player.position.y = screenHeight;
 
@@ -84,7 +113,6 @@ int main(void) {
             player.position.y -= player.velocity;
         } else {
             update_player(&player, screenWidth);
-            //update_foe(&target1, screenWidth);
         }
 
         shootTimer += GetFrameTime();
@@ -92,7 +120,7 @@ int main(void) {
         if (shootTimer > 0.1) {
             for (int i = 0; i < sizeof(bullets) / sizeof(bullets[0]); i++) {
                 if (bullets[i].active == 0) {
-                    bullets[i].position.x = player.position.x + player.size.x / 2;
+                    bullets[i].position.x = player.position.x + player.size.x / 2 - bullets[i].size.x / 2;
                     bullets[i].position.y = player.position.y - 20;
                     bullets[i].size = (Vector2){.x = 5, .y = 20};
                     bullets[i].velocity = 5;
@@ -115,12 +143,12 @@ int main(void) {
                 }
                 
                 
-                int isHit = CheckCollisionRecs(RectangleFromVector2(bullets[i].position, bullets[i].size), RectangleFromVector2(target1.position, target1.size));
+                //int isHit = CheckCollisionRecs(RectangleFromVector2(bullets[i].position, bullets[i].size), RectangleFromVector2(foes[i].position, target1.size));
                 
-                if(isHit){
-                    target1.health -= bullets[i].damage;
-                    bullets[i].active = 0;
-                }
+                //if(isHit && foes[i].health != 0){
+                    //foes[i].health -= bullets[i].damage;
+                    //bullets[i].active = 0;
+                //}
             }
         }
         BeginDrawing();
@@ -128,7 +156,13 @@ int main(void) {
             //DrawText(text, screenWidth / 2 - textWidth / 2, 200, 16, BLACK);
             
             DrawRectangle(player.position.x, player.position.y, player.size.x, player.size.y, BLUE);
-            DrawRectangle(target1.position.x, target1.position.y, target1.size.x, target1.size.y, RED);
+           
+            for(int i = 0; i < sizeof(foes) / sizeof(foes[0]); i++) {
+                if(foes[i].health > 0) {
+                    //DrawRectangle(foes[i].position.x, foes[i].position.y, foes[i].size.x, ., Color color)
+                    DrawRectangleV(foes[i].position, foes[i].size, RED);
+                }
+            }
 
             for (int i = 0; i < sizeof(bullets) / sizeof(bullets[0]); i++) {
                 if (bullets[i].active == 1) {
